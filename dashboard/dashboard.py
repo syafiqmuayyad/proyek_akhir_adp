2ff128d89d809d5e8410aa35d2453e82940a357d
Syafiq Muayyad <115983629+syafiqmuayyad@users.noreply.github.com>
2024-10-04 23:06:44 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv(mount/src/proyek_akhir_adp/dashboard/main_data.csv)
+data = pd.read_csv('dashboard/main_data.csv')
 
 st.title("Bike Sharing Dashboard")
 
